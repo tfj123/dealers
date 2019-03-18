81600a5c36ee98c39978ffe9a7a6ca545b006ba3
tianfj <1870609409@qq.com>
2019-03-18 11:11:39 +0800

    代理后台

---
+++ server/configs.js
@@ -0,0 +1,21 @@
+exports.mysql = function(){
+	return {
+		HOST:'127.0.0.1',
+		USER:'root',
+		PSWD:'123456',
+		DB:'tdkdk_game',//需要修改的数据库
+		PORT:3306,
+	}
+}
+
+exports.port = 12580;
+
+exports.users = function(){
+	return {
+		HOST:'localhost',
+		PORT:12581,
+		GET_PATH:'/get_user_info',
+		ADD_PATH:'/add_user_gems',
+		SAFE:false,		
+	}
+}
